911a0da150e6a7c3e0f8746a6ada43390128510e
Shravan Patel <patelshravan24@gmail.com>
2022-10-05 22:09:01 +0530

    update transaction query

--- sql/sampleData.sql
+++ sql/sampleData.sql
@@ -522,16 +522,19 @@ SET @deliveries2Id = LAST_INSERT_ID();
 
 -- FundsTransfer Query
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `FundsTransfer`()
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FundsTransfer`(amount float, account_to VARCHAR(50), account_from VARCHAR(50), timestamp DATETIME)
 BEGIN
-START TRANSACTION;
-    SET @Amount=200;
-    SET @FromAccount="45615156165";
-    SET @ToAccount="456151126165";
-    INSERT INTO transactions (to_account, from_account, amount, created_at) VALUES (@ToAccount, @FromAccount, @Amount, "2021-09-26"); 
-    UPDATE accounts SET balance=balance-@Amount WHERE account_number=@FromAccount;
-    UPDATE accounts SET balance=balance+@Amount WHERE account_number=@ToAccount;
-COMMIT;
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		ROLLBACK;
+	END;
+	START TRANSACTION;
+		INSERT INTO transactions (to_account, from_account, amount, created_at) VALUES (account_to, account_from, amount, timestamp); 
+		UPDATE accounts SET balance=balance-amount WHERE account_number=account_from;
+		UPDATE accounts SET balance=balance+amount WHERE account_number=account_to;
+	COMMIT;
 END;
 
-CALL FundsTransfer
+
+CALL FundsTransfer(200, "456151126165", "45615156165" ,"2021-09-26");
