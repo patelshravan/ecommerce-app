71df41fab11c0d51b5c1632bfef1084bd2b0d52a
Shravan Patel <patelshravan24@gmail.com>
2022-10-09 18:35:55 +0530

    business query updated

--- sql/businessQueries.sql
+++ sql/businessQueries.sql
@@ -1,20 +1,18 @@
 -- ALL BUSINESS RELATED QUERIES
 USE ecommerce;
+
 -- GET ALL CUSTOMERS
-SELECT *
-FROM customers;
+SELECT * FROM customers;
+
 -- GET CUSTOMER BY ID
-SELECT *
-FROM customers
-WHERE id = 1;
+SELECT * FROM customers WHERE id = 1;
+
 -- REMOVE CUSTOMER
-DELETE FROM customers
-Where id = 2;
+DELETE FROM customers Where id = 2;
+
 -- UPDATE CUSTOMER
-UPDATE customers
-SET contact_no = "7742026070",
-    modified_at = "2022-09-24"
-WHERE id = "1";
+UPDATE customers SET contact_no = "7742026070", modified_at = "2022-09-24" WHERE id = "1";
+
 -- GET CUSTOMER PERSONAL PROFILE
 SELECT customers.firstname as firstname,
     customers.email as email,
@@ -41,19 +39,19 @@ WHERE customers.id = 1
     AND orders.id = payments.order_id
     AND orders.customer_id = customers.id
     AND orders.id = ordersData.order_id;
+
 -- GET ALL SELLERS
-SELECT *
-FROM sellers;
+SELECT * FROM sellers;
+
 -- GET SELLERS BY ID
-SELECT *
-FROM sellers
-WHERE id = "1" -- REMOVE SELLER
-DELETE FROM sellers
-Where id = "1" -- UPDATE SELLER
-UPDATE sellers
-SET contact_no = "7894563210",
-    modified_at = "2021-02-21"
-WHERE id = "1";
+SELECT * FROM sellers WHERE id = "1" 
+
+-- REMOVE SELLER
+DELETE FROM sellers Where id = "1" 
+
+-- UPDATE SELLER
+UPDATE sellers SET contact_no = "7894563210", modified_at = "2021-02-21" WHERE id = "1";
+
 -- GET SELLER PROFILE
 SELECT name,
     location,
@@ -70,6 +68,7 @@ FROM sellers
     JOIN products ON sellers.id = products.sellers_id
 WHERE sellers.id = 1
     AND sellers.id = products.sellers_id;
+
 -- GET SELLER ORDERS
 SELECT sellers.name as sellers_name,
     sellers.email as sellers_email,
@@ -87,34 +86,40 @@ WHERE sellers.id = 1
     AND ordersData.order_id = orders.id
     AND products.id = ordersData.product_id
     AND sellers.id = products.sellers_id;
+
 -- GET VENDOR PROFILE
 SELECT name,
     email,
     contact_no
 FROM vendors;
+
 -- GET PRODUCTS WHERE PRICE > 100
 SELECT title,
     id,
     quantity
 FROM products
 WHERE price > 100;
+
 -- GET ALL AVAILABLE PRODUCTS
 SELECT title,
     id,
     quantity
 FROM products
 WHERE quantity > 0;
+
 -- GET UNAVAILABLE PRODUCTS
 SELECT title,
     id,
     quantity
 FROM products
 WHERE quantity = 0;
+
 -- GET ORDER LIST
 SELECT status,
     COUNT(*) AS count
 FROM orders
 GROUP BY status;
+
 -- GET categories LIST
 SELECT products.categories_id,
     categories.name,
@@ -122,3 +127,21 @@ SELECT products.categories_id,
 FROM products
     JOIN categories ON (products.categories_id = categories.id)
 GROUP BY products.categories_id;
+
+-- BUSINESS RELATED
+-- For Placing a Order
+
+INSERT INTO orders (status, customer_id, created_at, modified_at) VALUES ("placed", 1, "2022-01-02", "200-01-11"); --insert into orders
+
+SET @order_id = LAST_INSERT_ID(); --get last used id
+INSERT INTO ordersdata (order_id, product_id, price, quantity, created_at, modified_at) VALUES (@order_id, 1, 2000, 100,"2022-01-11 ", "2022-01-11") --insert into ordersdata
+
+UPDATE products SET quantity = quantity - 2 WHERE id = 1; --update products table
+       
+INSERT INTO payments (total_amount, discount_percentage,payable_amount,mode_of_payment, order_id, created_at, modified_at) VALUES (1000, 10 ,900, "UPI", @order_id, "2022-01-02", "2022-01-02"); --inert into payments
+
+-- For Paymnets Handling
+
+INSERT INTO payments(total_amount,discount_percentage,payable_amount,order_id,mode_of_payment,created_at,modified_at) values(1500,10,1350,1,"netbanking","2022-01-02","2022-01-02");
+
+UPDATE orders SET paid = true; WHERE id=1;
